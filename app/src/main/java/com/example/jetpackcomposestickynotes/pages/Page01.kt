81ea5f6c19660525294dfc10c5721b769c69d09e
shahb <malikshahbazzafar@gmail.com>
2023-07-17 15:58:58 +0500

    Added Page 01

---
+++ app/src/main/java/com/example/jetpackcomposestickynotes/pages/Page01.kt
@@ -0,0 +1,22 @@
+package com.example.jetpackcomposestickynotes.pages
+
+//(1) Android Native Ui Toolkit written in Kotlin
+
+//(2) Not part of the framework, it is a jetpack library
+
+//(3) Views was Android Framework Part but it is not part of android framework
+
+//(4) Imperative vs Declarative => Imperative is How and Declarative is What
+
+//(5) All the views inherit from the View Class. For large codebase inheritance becomes complex
+
+//(6) Composition over inheritance
+
+//(7) Recomposition => In simple words. Whenever your state changes, It will recreate the Ui
+
+//(8) For compose project minimum SDK is 21
+
+//(9) Remember: In Gradle file compose compiler version and android kotlin version both are linked each other
+//https://developer.android.com/jetpack/androidx/releases/compose-kotlin
+
+//(10) We can use Compose in XML Layouts with ComposeView Tag in XML
---
+++ app/src/main/java/com/example/jetpackcomposestickynotes/topics/Recomposition.kt
@@ -0,0 +1,7 @@
+package com.example.jetpackcomposestickynotes.topics
+
+//Topic Recomposition
+
+//Definition=>
+//In simple words. Whenever your state changes, It will recreate the Ui
+
